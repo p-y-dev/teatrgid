23b033075f3d2da61d02686489c34da1ba90696c
Pavel <yakovlev-pavel2@yandex.ru>
2017-03-27 00:13:29 +0700

    Finish footer for a resolution of at least 1200 px

--- frontend/static/js/home/home.js
+++ frontend/static/js/home/home.js
@@ -1,5 +1,22 @@
 (function() {
-    $(document).ready(function() {
+    function openFilter() {
+        $("#open-filter").click(function(){
+            var filter_obj = $(".filter");
+            var btn_open_obj = $("#open-filter");
 
+            if(filter_obj.hasClass("open")) {
+                filter_obj.removeClass("open");
+                filter_obj.css("display", "none");
+                btn_open_obj.text("Открыть фильтр");
+            } else {
+                filter_obj.css("display", "block");
+                filter_obj.addClass("open");
+                btn_open_obj.text("Закрыть фильтр");
+            }
+        })
+    }
+
+    $(document).ready(function() {
+        openFilter();
     });
 })($);
--- src/teatrgid/views.py
+++ src/teatrgid/views.py
@@ -2,7 +2,7 @@ from django.views.generic.base import TemplateView
 
 
 class HomePage(TemplateView):
-    template_name = "index.html"
+    template_name = "home/index.html"
 
     def get_context_data(self, **kwargs):
         context = super(HomePage, self).get_context_data(**kwargs)
